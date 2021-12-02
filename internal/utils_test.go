59a5d8a2f9bd79c1f56ea8075ba8e6362c493f13
thomaswei <thomas.wei@nadisystem.com>
2021-12-02 11:00:25 +0800

    refactoring test

--- internal/utils_test.go
+++ internal/utils_test.go
@@ -4,10 +4,10 @@ import (
 	"IAM/pkg/cache"
 	"IAM/pkg/password"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-//var db, err = sql.Open("mysql", "thomas:123456@/iam?charset=utf8&parseTime=true")
 var _, _ = db.Exec("CREATE TABLE IF NOT EXISTS `accounts`\n(\n    `id`       int          NOT NULL AUTO_INCREMENT,\n    `username` VARCHAR(30)  NOT NULL,\n    `password` VARCHAR(100) NOT NULL,\n    `email`    VARCHAR(50)  not NULL,\n    `auth`     INT          not NULL,\n    UNIQUE (`username`),\n    PRIMARY KEY (`id`)\n);")
 var _, _ = db.Exec("INSERT accounts\nSET username='Admin',\n    password='$2a$10$dN7Da733DxGG4CLLfRQ.5OV8UakM8H1yo5o1aWj9uOGPSBU7ZmmY6',\n    Email='admin@admin.com',\n    Auth=255;")
 var _, _ = db.Exec("INSERT accounts\nSET username='Manager',\n    password='$2a$10$dN7Da733DxGG4CLLfRQ.5OV8UakM8H1yo5o1aWj9uOGPSBU7ZmmY6',\n    Email='manager@admin.com',\n    Auth=200;")
@@ -19,22 +19,15 @@ func TestSignUp(t *testing.T) {
 	auth := 123
 
 	got, err := SignUp(username, password, email, auth)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
-	rows, err := db.Query("SELECT id FROM accounts where username='test!!!'")
+	require.NoError(t, err)
 
+	rows, err := db.Query("SELECT id FROM accounts where username='test!!!'")
 	var want int64
 	for rows.Next() {
 		err = rows.Scan(&want)
 	}
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if got != want {
-		t.Errorf("ID mismatch, create account fail")
-	}
+	require.NoError(t, err)
+	require.Equal(t, want, got)
 
 	DeleteAccount(int32(got))
 }
@@ -43,63 +36,44 @@ func TestLogin(t *testing.T) {
 	username := "Admin"
 	password := "123456"
 	token, err := Login(username, password)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	value, err := cache.CacheGet(token)
 	value2 := value.(cache.TokenValue)
-	if value2.Id != 1 {
-		t.Errorf("not get correct token from cache")
-		return
-	}
+	require.Equalf(t, 1, value2.Id, "not correct token from cache")
 }
 
 func TestLogout(t *testing.T) {
 	username := "Admin"
-	password := "123456"
-	token, err := Login(username, password)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	passWord := "123456"
+	token, err := Login(username, passWord)
+	require.NoError(t, err)
+
 	err = Logout(token)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	_, err = cache.CacheGet(token)
-	if err == nil {
-		t.Errorf("remove token fail")
-	}
+	require.NoErrorf(t, err, "remove token fail")
 
 }
 
 func TestTokenverify(t *testing.T) {
 	username := "Admin"
-	password := "123456"
-	token, err := Login(username, password)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	passWord := "123456"
+	token, err := Login(username, passWord)
+	require.NoError(t, err)
+
 	_, got, err := Tokenverify(token)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	want := 255
-	if got != want {
-		t.Errorf("token verify error, auth got %v, want %v", got, want)
-	}
+	require.Equalf(t, want, got, "token verify error, auth got %v, want %v", got, want)
 }
 
 func TestGetAllAccount(t *testing.T) {
 	allAccounts, err := GetAllAccount()
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	want := "admin@admin.com"
 	got := ""
 	for _, account := range allAccounts {
@@ -107,9 +81,7 @@ func TestGetAllAccount(t *testing.T) {
 			got = account.Email
 		}
 	}
-	if got != want {
-		t.Errorf("email not match , got %v, want %v", got, want)
-	}
+	require.Equalf(t, want, got, "email not match , got %v, want %v", got, want)
 }
 
 func TestUpdateSingelAccount(t *testing.T) {
@@ -121,68 +93,51 @@ func TestUpdateSingelAccount(t *testing.T) {
 	want := 100
 	var got int
 	err := UpdateSingelAccount(fields)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	rows, err := db.Query("SELECT auth FROM accounts where id=1")
 	for rows.Next() {
 		err = rows.Scan(&got)
 	}
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-	if got != want {
-		t.Errorf("update fail, auth not match , got %v, want %v", got, want)
-	}
+	require.NoError(t, err)
+	require.Equalf(t, want, got, "update fail, auth not match , got %v, want %v", got, want)
+
 	// 通過測試後改回原數據
 	fields["Auth"] = 255
 	err = UpdateSingelAccount(fields)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
 }
 
 func TestRenewPassword(t *testing.T) {
 	want := "1qaz@WSX3edcZ012"
 	err := ChangeSelfPassword(2, want)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	var got string
 	rows, err := db.Query("SELECT password FROM accounts where id=2")
 	for rows.Next() {
 		err = rows.Scan(&got)
 	}
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+	require.NoError(t, err)
+
 	err = password.CheckPassword(want, got)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+	require.NoError(t, err)
+
 }
 
 func TestInitPassword(t *testing.T) {
 	want := "123456"
 	err := InitPassword(2, want)
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
+	require.NoError(t, err)
+
 	var got string
 	rows, err := db.Query("SELECT password FROM accounts where id=2")
 	for rows.Next() {
 		err = rows.Scan(&got)
 	}
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+	require.NoError(t, err)
+
 	err = password.CheckPassword(want, got)
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+	require.NoError(t, err)
 
 }
